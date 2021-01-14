d98bfd37c884a57d1cd7f87c736753ee74713bed
danielmdezfdez <63611815+danielmdezfdez@users.noreply.github.com>
2021-01-14 16:49:13 +0100

    Updated Classes

--- modules/estima.py
+++ modules/estima.py
@@ -8,9 +8,9 @@ class Angle():
     def __init__(self):
         pass
 
-    def sexagesimal_user_input(self,):
+    def sexagesimal_user_input(self, valor_entrada):
 
-        x=input(message)
+        x=input(valor_entrada)
 
         degrees, minutes=x.split()
         degrees=float(degrees)
@@ -23,9 +23,6 @@ class Angle():
 
         return decimal_degrees
 
-
-
-
 class Position():
     'Class to define vessel position'
     def __init__(self): # Inicializa la clase
@@ -33,14 +30,16 @@ class Position():
 
     def get_position(self): # Pide al usuario que introduzca una latitud y longitud
         'Asks user for a new position on decimal coordinates which is returned on a tuple'
-        self.latitude=(sexagesimal_user_input("Latitude: "))
-        self.longitude=(sexagesimal_user_input("Longitude: "))
+
+        self.latitude = Angle.sexagesimal_user_input(self, "Latitude: ")
+
+        self.longitude = Angle.sexagesimal_user_input(self, "Longitude: ")
 
         self.departure_position = [self.latitude, self.longitude]
 
         return [self.latitude, self.longitude]
 
-"""  
+
 class Estima():
     def __init__(self):
         pass
@@ -48,27 +47,23 @@ class Estima():
     def calculation(self, position):
 
         self.departure_position = Position.get_position(position)
-
-        self.departure_latitude = self.departure_position[0]
-        
+        self.departure_latitude = self.departure_position[0]        
         self.departure_longitude = self.departure_position[1]
+
 
         self.ellapsed_time = float(input("Ellapsed Time (HH:MM:SS): "))
         self.course = float(input("Course: "))
         self.vessel_speed = float(input("Vessel Speed (kn): "))
-
         self.distance = self.ellapsed_time * self.vessel_speed
 
+
         self.delta_latitude = (self.distance/60)*math.cos(self.course)
-
-        self.apartamiento = (self.distance/60)*math.sin(self.course)
-
         self.arrival_latitude = self.departure_latitude + self.delta_latitude
 
+
         self.latitude_average = self.departure_latitude + self.arrival_latitude
-
+        self.apartamiento = (self.distance/60)*math.sin(self.course)
         self.delta_longitude = self.apartamiento / math.cos(self.latitude_average)
-
         self.arrival_longitude = self.departure_longitude + self.delta_longitude
         
         print([self.arrival_latitude, self.arrival_longitude])
@@ -76,24 +71,6 @@ class Estima():
 
         return [self.arrival_latitude, self.arrival_longitude]
 
-"""
-"""     
-departure_point = Position()
 
-arrival_point = Position()
-
-CalculoPrimero = Estima()
-
-
-
-Estima.calculation(CalculoPrimero, departure_point)
-
-
-
-"""
-
-random_point = Position()
-
-sexagesimal_user_input()
 
 
