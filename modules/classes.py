524054a1d1e6cb0e1612bc63fb7cbafa66a9bff7
danielmdezfdez <63611815+danielmdezfdez@users.noreply.github.com>
2021-01-24 21:47:46 +0100

    splited methods and classes into two files

--- modules/classes.py
+++ modules/classes.py
@@ -98,26 +98,6 @@ class DeadReckoning(Vessel):
         return [self.latitude_difference, self.longitude_difference]
 
 
-# Cálculo de Estima
-
-
-
-Salida = Position()                     # Creates Departure Point within Position class.
-Position.get_user_position(Salida)      # Defines Departure Point within Position class.
-
-
-OwnVessel = Vessel()                    # Creates ownVessel for Vessel Data entry.
-Vessel.speed_and_course(OwnVessel)      # Defines ownVessel parameters
-
-sailed_time = Time()
-sailed_time = Time.get_user_time(Time)
-
-
-Calculo = DeadReckoning(OwnVessel)                                   # Creates "calc" for Estima Calculations
-DeadReckoning.direct(Calculo, Salida, OwnVessel, sailed_time)        # Calculates Dead Reckoning Parameters
-
-Llegada = Position()
-
 
 
 
--- modules/main.py
+++ modules/main.py
@@ -1,16 +1,22 @@
-from classes import Angle, Position, DeadReckoning
+from classes import *
 
-Salida = Position()
+# Cálculo de Estima
+
+
+
+Salida = Position()                     # Creates Departure Point within Position class.
+Position.get_user_position(Salida)      # Defines Departure Point within Position class.
+
+
+OwnVessel = Vessel()                    # Creates ownVessel for Vessel Data entry.
+Vessel.speed_and_course(OwnVessel)      # Defines ownVessel parameters
+
+sailed_time = Time()
+sailed_time = Time.get_user_time(Time)
+
+
+Calculo = DeadReckoning(OwnVessel)                                   # Creates "calc" for Estima Calculations
+DeadReckoning.direct(Calculo, Salida, OwnVessel, sailed_time)        # Calculates Dead Reckoning Parameters
 
 Llegada = Position()
 
-Estima = DeadReckoning()
-
-Salida = Position.get_position(Salida)
-
-Llegada = DeadReckoning.dead_reckoning(Estima, Salida)
-
-
-Position.position_console_print(Salida)
-
-Position.position_console_print(Llegada)
